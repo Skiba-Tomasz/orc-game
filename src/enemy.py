3846beff59a37f058638060b25a49f816a632efe
Tomasz Skiba <skibielec@gmail.com>
2021-01-21 02:06:21 +0100

    Clean enemy

--- src/enemy.py
+++ src/enemy.py
@@ -18,27 +18,29 @@ class Enemy(Sprite, Attackable):
 		self.isMoving = False
 		self.direction = Direction.DOWN
 		self.spriteSheet = pygame.image.load('../img/goblins.png')
-		self.prapareSprite(1)
+		self.__prapareSprite(1)
 		self.rect = self.image.get_rect()
 		self.rect.x = position[0]*48
 		self.rect.y = position[1]*48
 		self.path = []
 		self.nextMove = None
 
-	def prapareSprite(self, frame):
-		size = self.spriteSize
-		self.image = Surface((25,25))
-		spTopLeft = size * self.spriteID * 3
-		spX = spTopLeft + size * frame
-		spY = self.direction.value * size
-		self.image.blit(self.spriteSheet, (0, 0), (spX+11, spY+23, 25, 25))
-		self.image = pygame.transform.scale(self.image, (size,size))
-		self.image.set_colorkey((0, 0, 0))
+	def isPathCompleted(self):
+		if (self.path is None or len(self.path) == 0) and self.nextMove is None and self.isReadyForNewPath():
+			return True
+		return False
 
+	def isReadyForNewPath(self):
+		if self.rect.x % 48 == 0 and self.rect.y % 48 == 0:
+			return True
+		return False
 
+	def setPath(self, path):
+		self.path = path
+		self.nextMove = None
 
 	def calculateState(self):
-		self.setMoveFromPath()				
+		self.__setMoveFromPath()				
 		if self.isMoving:
 			self.frame += 1
 			if self.frame == 3:
@@ -53,41 +55,7 @@ class Enemy(Sprite, Attackable):
 				self.rect.x -= self.speed
 		else:
 			self.frame = Enemy.STATIONARY_FRAME
-		self.prapareSprite(self.frame)
-
-	def setMoveFromPath(self):
-		if self.path is not None and len(self.path) > 0 and self.nextMove is None:
-			if len(self.path) > 1 and self.getMoveDirection(self.path[1].x, self.path[1].y) == self.direction:
-				self.path.pop(0)
-			self.nextMove = self.path.pop(0)
-		if self.nextMove is not None:
-			xTo = self.nextMove.x * 48
-			yTo = self.nextMove.y * 48
-			#print('Enemy position(' + str(self.rect.x) + 'x' + str(self.rect.y) + ' Target position (' + str(xTo) + 'x' + str(yTo) + ')')
-			self.isMoving = True
-			self.direction = self.getMoveDirection(xTo, yTo)
-			if self.rect.x == xTo and self.rect.y == yTo:
-				print('Position reached (' + str(self.rect.x) + 'x' + str(self.rect.y) + ' Target position (' + str(xTo) + 'x' + str(yTo) + ')')
-				self.nextMove = None
-				self.isMoving = False
-				self.setMoveFromPath()
-
-	def getMoveDirection(self, xTo, yTo):
-		direction = self.direction
-		if xTo < self.rect.x:
-			direction = Direction.LEFT
-		elif xTo > self.rect.x:
-			direction = Direction.RIGHT
-		elif yTo < self.rect.y:
-			direction = Direction.UP
-		elif yTo > self.rect.y:
-			direction = Direction.DOWN
-		return direction
-
-	def setPath(self, path):
-		self.path = path
-		self.nextMove = None
-
+		self.__prapareSprite(self.frame)
 
 	def calculateCollisions(self, colliders):
 		walls = pygame.sprite.Group()
@@ -96,11 +64,6 @@ class Enemy(Sprite, Attackable):
 				walls.add(ob)
 		self.onWallCollision(walls)
 		walls.empty()
-
-	def isReadyForNewPath(self):
-		if self.rect.x % 48 == 0 and self.rect.y % 48 == 0:
-			return True
-		return False
 
 	def onWallCollision(self, walls):
 		collisions = pygame.sprite.spritecollide(self, walls, False)
@@ -118,7 +81,41 @@ class Enemy(Sprite, Attackable):
 					self.rect.x = collision.rect.x + collision.rect.width
 				collisions.clear()
 
-	def isPathCompleted(self):
-		if (self.path is None or len(self.path) == 0) and self.nextMove is None and self.isReadyForNewPath():
-			return True
-		return False
+	def __prapareSprite(self, frame):
+		size = self.spriteSize
+		self.image = Surface((25,25))
+		spTopLeft = size * self.spriteID * 3
+		spX = spTopLeft + size * frame
+		spY = self.direction.value * size
+		self.image.blit(self.spriteSheet, (0, 0), (spX+11, spY+23, 25, 25))
+		self.image = pygame.transform.scale(self.image, (size,size))
+		self.image.set_colorkey((0, 0, 0))
+
+	def __setMoveFromPath(self):
+		if self.path is not None and len(self.path) > 0 and self.nextMove is None:
+			if len(self.path) > 1 and self.__getMoveDirection(self.path[1].x, self.path[1].y) == self.direction:
+				self.path.pop(0)
+			self.nextMove = self.path.pop(0)
+		if self.nextMove is not None:
+			xTo = self.nextMove.x * 48
+			yTo = self.nextMove.y * 48
+			#print('Enemy position(' + str(self.rect.x) + 'x' + str(self.rect.y) + ' Target position (' + str(xTo) + 'x' + str(yTo) + ')')
+			self.isMoving = True
+			self.direction = self.__getMoveDirection(xTo, yTo)
+			if self.rect.x == xTo and self.rect.y == yTo:
+				print('Position reached (' + str(self.rect.x) + 'x' + str(self.rect.y) + ' Target position (' + str(xTo) + 'x' + str(yTo) + ')')
+				self.nextMove = None
+				self.isMoving = False
+				self.__setMoveFromPath()
+
+	def __getMoveDirection(self, xTo, yTo):
+		direction = self.direction
+		if xTo < self.rect.x:
+			direction = Direction.LEFT
+		elif xTo > self.rect.x:
+			direction = Direction.RIGHT
+		elif yTo < self.rect.y:
+			direction = Direction.UP
+		elif yTo > self.rect.y:
+			direction = Direction.DOWN
+		return direction
